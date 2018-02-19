2d6f0ede04b0c5a179d752899a1d7657b41af4ff
alex <alex@DESKTOP-2HO1N3I.fios-router.home>
2018-02-19 03:17:05 -0500

    Add exercise 4 and remove unused libraries

--- Competitive Programming 3/src/exercises/ex1_2_3.java
+++ Competitive Programming 3/src/exercises/ex1_2_3.java
@@ -1,8 +1,7 @@
 package exercises;
 
 import java.time.Month;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ex1_2_3 {
@@ -10,6 +9,7 @@ public class ex1_2_3 {
 		//ex1();
 		//ex2();
 		//ex3();
+		//ex4();
 	}
 	
 	public static void ex1() {
@@ -36,5 +36,13 @@ public class ex1_2_3 {
 		int[] t = new int[] {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
 		y -= m < 3 ? 1 : 0;
 		System.out.println((y + y/4 - y/100 + y/400 + t[m-1] + d)%7);
+	}
+	
+	public static void ex4() {
+		Scanner s = new Scanner(System.in);
+		int[] nums = Arrays.stream(s.nextLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+		for (int num : nums) {
+			System.out.print(num + " ");
+		}
 	}
 }
