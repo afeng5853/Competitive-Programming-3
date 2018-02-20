260a1f7a2c8e3c1d7d7fb490ca22a705028bf689
alex <alex@DESKTOP-2HO1N3I.fios-router.home>
2018-02-20 03:28:02 -0500

    Add more exercises

--- Competitive Programming 3/src/exercises/ex1_2_3.java
+++ Competitive Programming 3/src/exercises/ex1_2_3.java
@@ -1,15 +1,30 @@
 package exercises;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
 import java.time.Month;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ex1_2_3 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//ex1();
 		//ex2();
 		//ex3();
-		ex4();
+		//ex4();
+		//ex5();
+		//ex6();
+		//ex7("", 0);
+		ex8();
 	}
 	
 	public static void ex1() {
@@ -43,6 +58,91 @@ public class ex1_2_3 {
 		int[] nums = Arrays.stream(s.nextLine().split(" ")).mapToInt(Integer::parseInt).distinct().sorted().toArray();
 		for (int num : nums) {
 			System.out.print(num + " ");
+		}
+	}
+	
+	public static void ex5() {
+		// input as DD MM YYYY
+		// sort by month, then day, then age
+		Scanner s = new Scanner(System.in);
+		int n = Integer.parseInt(s.nextLine());
+		int[][] birthdayContainer = new int[n][];
+		for (int i = 0; i < n; i++) {
+			String line = s.nextLine();
+			int[] nums = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+			birthdayContainer[i] = nums;
+		}
+		Arrays.sort(birthdayContainer, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return Integer.compare(o1[1], o2[1]);
+			}
+		});
+		Arrays.sort(birthdayContainer, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return Integer.compare(o1[0], o2[0]);
+			}
+		});
+		Arrays.sort(birthdayContainer, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				int age1 = Period.between(LocalDate.of(o1[2], o1[1], o1[0]), LocalDate.now()).getYears();
+				int age2 = Period.between(LocalDate.of(o2[2], o2[1], o2[0]), LocalDate.now()).getYears();
+				return Integer.compare(age1, age2);
+			}
+		});
+		for (int[] date : birthdayContainer) {
+			System.out.println(date[0] + " " + date[1] + " " + date[2]);
+		}
+	}
+	
+	public static void ex6() throws IOException {
+		BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+		BitSet bm = new BitSet();
+		Arrays.stream(s.readLine().split("\\s")).mapToInt(Integer::parseInt).forEach(e -> bm.set(e));
+		System.out.println(bm.get(Integer.parseInt(s.readLine())));
+	}
+	
+	/* Wrong, it said to generate all permutations of the first 10 letters, not combinations
+	private static String[] letters = new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+	public static String ex7(String hitherto, int length) {
+		if (length == 10) {
+			return hitherto;
+		}
+		for (String s : letters) {
+			String res = ex7(hitherto + s, length + 1);
+			if (res != null)
+				System.out.println(res);
+		}
+		return null;
+	}
+	*/
+	
+	private static List<Set<Integer>> powerset(int[] nums) {
+		int binaryRep = 1 << nums.length;
+		List<Set<Integer>> powerset = new ArrayList<Set<Integer>>();
+		for (int i = 0; i < binaryRep; i++) {
+			Set<Integer> set = new HashSet<Integer>();
+			for (int j = 0; j < nums.length; j++) {
+				if ((i & (1 << j)) == 0) {
+					set.add(nums[j]);
+				}
+			}
+			powerset.add(set);
+		}
+		return powerset;
+	}
+	
+	public static void ex8() throws IOException {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(r.readLine());
+		int[] nums = new int[N];
+		for (int i = 0; i < N; i++) {
+			nums[i] = i;
+		}
+		for (Set<Integer> set : powerset(nums)) {
+			System.out.println(set);
 		}
 	}
 }
